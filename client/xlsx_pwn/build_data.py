432e4e60bd8cc1f0729fca9463f160b9f401ef20
Hao Li <hao.li@metabit-trading.com>
2023-04-05 15:02:52 +0800

    kup_sync

--- client/xlsx_pwn/build_data.py
+++ client/xlsx_pwn/build_data.py
@@ -2,8 +2,10 @@ import itertools
 import sys
 
 with open("data.in", 'w+') as file:
-    sys.stdout = file
+    # sys.stdout = file
     for i in range(0,10000000):
+        file.write(str(i))
         data = str(i).zfill(6)
-        print(data,end=" ")
+        file.write(str(i)+" ")
+        # print(i,end=" ")
         
